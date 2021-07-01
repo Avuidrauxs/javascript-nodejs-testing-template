68a6dc79eb26699640b4ad1cc25937776d1eef66
tepeszCodes <54599709+tepeszCodes@users.noreply.github.com>
2021-07-01 16:20:18 +0200

    Create index.js
    added a condition in validateResults function to check if input is an integer

---
+++ index.js
@@ -0,0 +1,36 @@
+function gradeResults(results) {
+  if(!Array.isArray(results) || validateResults(results)) {
+      return 'Please enter an array with scores of data type number between 0 - 100'
+  }
+  const grades = results.map(result => gradeResult(result))
+  return grades
+}
+
+function validateResults(results) {
+    const found = results.find(result => typeof result !== 'number' || (result > 100 || result < 0) || !Number.isInteger(result) )
+    if (found) {
+        return true
+    }
+    return false
+}
+
+function gradeResult(result) {
+    if(result >= 80) {
+        return 'A'
+    } else if(result >= 70 && result <= 79) {
+        return 'B'
+    } else if(result >= 60 && result <= 69) {
+        return 'C'
+    } else if(result >= 50 && result <= 59) {
+        return 'D'
+    } else if(result < 40) {
+        return 'F'
+    }
+}
+
+
+module.exports = {
+    gradeResults,
+    gradeResult,
+    validateResults
+}
